6f71fae610d793828575b9a7c9a06f0443fdf387
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-08-19 16:17:42 +0530

    now sets the string directry from command line 'argv'

--- main.c
+++ main.c
@@ -7,11 +7,13 @@ int main(int argc, char const *argv[])
     string a;
     init_str(&a); // we need to pass it's refernce as it accepts only pointers to our string { aka __string__ }
 
-    a.set(&a, "Hello");
-
+    for (size_t i = 1; i < argc; i++)
+    {
+        a.append(&a, argv[i]);
+        if (i < argc - 1)
+            a.append(&a, " ");
+    }
     a.print(&a, true);
-
-    // Do not free 'a' because it is not stored in heap memory and it is not a pointer.
-    // It is freed automatically when `int main(int argc, char const *argv[])` returns 0;
+    a.destructor(&a);
     return 0;
 }
