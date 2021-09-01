bbacea0e3fde05246375ad39fcea9ba26514768e
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-01 23:12:41 +0530

    moved `entropy` after printing `a` added `length` info to be printed

--- test.c
+++ test.c
@@ -7,8 +7,9 @@ int main(int argc, char const *argv[])
     sstring a;
     init_str(&a); // we need to pass it's refernce as it accepts only pointers to our string { aka __string__ }
     a.set_array(&a, argv, ' ', 1, argc, argc);
-    printf("Entropy = %Lf\n", a.entropy(&a));
     a.print(&a, true, "");
+    printf("Entropy = %Lf\n", a.entropy(&a));
+    printf("Length = %lu\n", a.length(&a));
     a.destructor(&a);
     return 0;
 }
