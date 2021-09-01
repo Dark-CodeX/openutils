647106e34659bcbb08119861f526008d2989ba55
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-01 21:53:26 +0530

    implemented `set_array` function

--- test.c
+++ test.c
@@ -6,12 +6,7 @@ int main(int argc, char const *argv[])
 {
     string a;
     init_str(&a); // we need to pass it's refernce as it accepts only pointers to our string { aka __string__ }
-    for (size_t i = 1; i < argc; i++)
-    {
-        a.append(&a, argv[i]);
-        if (i < argc - 1)
-            a.append(&a, " ");
-    };
+    a.set_array(&a, argv, ' ', 1, argc, argc);
     printf("Entropy = %Lf\n", a.entropy(&a));
     a.print(&a, true, "");
     a.destructor(&a);
