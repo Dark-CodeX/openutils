dc4908def48e3b7375bad3b78b66546752a71588
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-01 22:56:32 +0530

    renamed string to sstring

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char const *argv[])
 {
-    string a;
+    sstring a;
     init_str(&a); // we need to pass it's refernce as it accepts only pointers to our string { aka __string__ }
     a.set_array(&a, argv, ' ', 1, argc, argc);
     printf("Entropy = %Lf\n", a.entropy(&a));
