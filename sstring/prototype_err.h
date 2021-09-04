3cabe8485b050f1495468ea5a74472d67c2c5deb
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-04 14:13:48 +0530

    added license info

--- sstring/prototype_err.h
+++ sstring/prototype_err.h
@@ -5,6 +5,7 @@
 * Commit to this repository at https://github.com/Dark-CodeX/SafeString.git
 * You can use this header file. Do not modify it locally, instead commit it on github.com
 * File: "prototype_err.h" under "sstring" directory
+* License: MIT
 * sstring: version 3.5.1
 */
 
