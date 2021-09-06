49940eb62f7c1e7687abaf4e34725ae99893a737
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-06 18:33:05 +0530

    added LICENSE description

--- sstring/sstring.h
+++ sstring/sstring.h
@@ -1,12 +1,32 @@
 /**
 * This header file is written to manage string data safely under C programming language.
 * Author: Tushar Chaurasia (https://github.com/Dark-CodeX/)
-* Copyright: Tushar Chaurasia
 * Commit to this repository at https://github.com/Dark-CodeX/SafeString.git
-* You can use this header file. Do not modify it locally, instead commit it on github.com
+* You can use this header file. Do not modify it locally, instead commit it on https://www.github.com
 * File: "sstring.h" under "sstring" directory
-* License: MIT
 * sstring: version 4.1.2
+* 
+* MIT License
+* 
+* Copyright (c) 2021 Tushar Chaurasia
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
 */
 typedef struct __string__ sstring;
 
