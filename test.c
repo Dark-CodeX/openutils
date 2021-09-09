e631db925c0233b713c462aca4dc3d2b4e737038
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-09 14:24:04 +0530

    optimized with new rules of `sstring`

--- test.c
+++ test.c
@@ -2,12 +2,9 @@
 
 int main(int argc, char const *argv[])
 {
-    sstring a;
-    init_sstr(&a); // we need to pass it's refernce as it accepts only pointers to our string { aka __string__ }
+    SSTRING(a);
     a.set_array(&a, argv, ' ', 1, argc, argc);
-    a.print(&a, true, "");
-    printf("Entropy = %Lf\n", a.entropy(&a));
-    printf("Length = %llu\n", a.length(&a));
+    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu", a.entropy(&a), a.length(&a));
     a.destructor(&a);
     return 0;
 }
