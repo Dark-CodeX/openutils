4789d930c2d83c9465e8b432478098a01e4fbfd7
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-18 15:40:40 +0530

    now using `soundex` function

--- test.c
+++ test.c
@@ -4,7 +4,9 @@ int main(int argc, char const **argv)
 {
     SSTRING(a);
     a.set_array(&a, argv, ' ', 1, argc, argc);
-    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu", a.entropy(&a), a.length(&a));
+    char *sdx = a.soundex(&a);
+    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu\nSoundex = %s\n", a.entropy(&a), a.length(&a), sdx);
+    free(sdx);
     a.destructor(&a);
     return 0;
 }
