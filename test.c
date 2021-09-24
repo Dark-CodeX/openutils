55e692870b57ffe6a645ba77adcec9df2045c62f
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-09-24 10:21:05 +0530

    removed `soundex` call

--- test.c
+++ test.c
@@ -4,9 +4,7 @@ int main(int argc, char const **argv)
 {
     SSTRING(a);
     a.set_array(&a, argv, ' ', 1, argc, argc);
-    char *sdx = a.soundex(&a);
-    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu\nSoundex = %s\n", a.entropy(&a), a.length(&a), sdx);
-    free(sdx);
+    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu", a.entropy(&a), a.length(&a));
     a.destructor(&a);
     return 0;
 }
