9dcb3b68ae7646b278e67f2260be1591a1563aac
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-10-07 08:55:51 +0530

    updated version

--- sstring/prototype_err.h
+++ sstring/prototype_err.h
@@ -4,7 +4,7 @@
 * Commit to this repository at https://github.com/Dark-CodeX/SafeString.git
 * You can use this header file. Do not modify it locally, instead commit it on https://www.github.com
 * File: "prototype_err.h" under "sstring" directory
-* sstring: version 17.0.10
+* sstring: version 17.1.0
 * 
 * MIT License
 * 
