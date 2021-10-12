c30464c3f173a337c2a4ad7a87b7b21afb0eb2b8
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-10-12 18:31:01 +0530

    now uses only a single for loop for all tasks

--- struct_test.c
+++ struct_test.c
@@ -5,14 +5,12 @@ int main(int argc, char const **argv)
 {
     vector(vec, sstring);
     for (SIZE_T i = 0; i < (SIZE_T)argc; i++)
-        vector_add(vec, new_sstring(NULL));
-    for (SIZE_T i = 0; i < (SIZE_T)argc; i++)
     {
+        vector_add(vec, new_sstring(NULL));
         vec.src[i].set(&vec.src[i], argv[i]);
         vec.src[i].print(&vec.src[i], true, "\t\tLength = %llu, Entropy = %Lf", vec.src[i].length(&vec.src[i]), vec.src[i].entropy(&vec.src[i]));
-    }
-    for (SIZE_T i = 0; i < (SIZE_T)argc; i++)
         vec.src[i].destructor(&vec.src[i]);
+    }
     vector_destructor(vec);
     return 0;
 }
