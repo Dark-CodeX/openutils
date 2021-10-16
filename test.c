eedd7658beefc7057c03cd297a166a9d07aa02e1
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-10-16 18:02:47 +0530

    uses `end` function for getting the length of `sstring`

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int main(int argc, char const **argv)
 {
     SSTRING(a);
     a.set_array(&a, argv, ' ', 1, argc, argc);
-    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu\nAvg = %Lf\n", a.entropy(&a), a.length(&a), a.positional_average(&a));
+    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu\nAvg = %Lf\n", a.entropy(&a), a.end(&a), a.positional_average(&a));
     a.destructor(&a);
     return 0;
 }
