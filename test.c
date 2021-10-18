a29c594f91ff76719d26b9b234e3424246eb8e52
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-10-18 22:10:56 +0530

    now uses `positional_average` function

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int main(int argc, char const **argv)
 {
     SSTRING(a);
     a.set_array(&a, argv, ' ', 1, argc, argc);
-    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu\nAvg = %Lf\n", a.entropy(&a), a.end(&a), a.positional_average(&a));
+    a.print(&a, true, "\nEntropy = %Lf\nLength = %llu\nPositional Average = %Lf\nPositional Modulus = %llu\n", a.entropy(&a), a.end(&a), a.positional_average(&a), a.positional_modulus(&a));
     a.destructor(&a);
     return 0;
 }
