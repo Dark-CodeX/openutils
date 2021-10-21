cd6ad63c89395f461ce543e980069fec6dc2a43e
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-10-21 16:40:13 +0530

    updated

--- struct_test.c
+++ struct_test.c
@@ -6,7 +6,7 @@ int main(int argc, char const **argv)
     vector(vec, sstring);
     for (SIZE_T i = 0; i < (SIZE_T)argc; i++)
     {
-        vector_add(vec, new_sstring(NULL));
+        vector_add(vec, new_sstring(1, NULL));
         vec.src[i].set(&vec.src[i], argv[i]);
         vec.src[i].print(&vec.src[i], true, "\t\tLength = %llu, Entropy = %Lf", vec.src[i].length(&vec.src[i]), vec.src[i].entropy(&vec.src[i]));
         vec.src[i].destructor(&vec.src[i]);
