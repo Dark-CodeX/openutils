a00f2528d8a74aa197f34d1df9a106f6380f02dc
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-11-09 08:38:16 +0530

    testing (UNDER DEVELOPMENT)

--- sstring/test.cc
+++ sstring/test.cc
@@ -1,14 +1,14 @@
 #include "sstring.hh"
 #include <iostream>
+#include <string>
 
 using namespace sstr;
 
 int main(void)
 {
-    sstring x;
+    sstring x = to_sstring(42);
     x.open("sstring/sstring.h");
-    split_t s;
-    s = x.split("\n");
+    split_t s = x.split("\n");
     for (std::size_t i = 0; i < s.length(); i++)
         std::cout << '`' << s[i] << '`' << std::endl;
     return 0;
