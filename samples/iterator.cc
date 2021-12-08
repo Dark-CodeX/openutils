34d5a96f267ffaaab5b375798853b99b8853ca05
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-12-08 09:59:35 +0530

    added reverse iterator testing

--- samples/iterator.cc
+++ samples/iterator.cc
@@ -4,7 +4,11 @@
 int main(void)
 {
     vector_t<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::cout << "Normal" << std::endl;
     for (vector_t<int>::iter i = v.iterator(); i.c_loop(); i.next())
+        std::cout << *i << std::endl;
+    std::cout << "Reverse" << std::endl;
+    for (vector_t<int>::iter i = v.reverse_iterator(); i.c_loop(); i.next())
         std::cout << *i << std::endl;
     return 0;
 }
