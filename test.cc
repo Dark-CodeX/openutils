b4f296fa4dde25225c97a0194b8a3dddb58f072c
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-12-11 23:44:39 +0530

    simple updates

--- test.cc
+++ test.cc
@@ -6,10 +6,11 @@ int main(int argc, char const **argv)
 {
     using namespace sstr;
     map_t<sstring, int> map;
-    for (std::size_t i = 1; i < argc; ++i)
+    for (std::size_t i = 1; i < (std::size_t)argc; ++i)
         map.add(argv[i], i);
-    for (map_t<sstring, int>::iter i = map.iterator(); i.c_loop(); i.next())
+    for (map_t<sstring, int>::iter i = map.iterator(); i.c_loop() != false; i.next())
         std::cout << i->key << " " << i->value << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
     std::cout << "Length = " << map.length() << std::endl;
     std::cout << "Capacity = " << map.capacity() << std::endl;
     std::cout << "HASH = " << std::hash<map_t<sstring, int>>()(map) << std::endl;
