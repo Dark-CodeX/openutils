70e2f54a0fdf81a8ad1083cd52167e27759e2d17
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2021-12-13 11:53:27 +0530

    updated version

--- sstring/binary.h
+++ sstring/binary.h
@@ -4,7 +4,7 @@
 * Commit to this repository at https://github.com/Dark-CodeX/SafeString.git
 * You can use this header file. Do not modify it locally, instead commit it on https://www.github.com
 * File: "binary.h" under "sstring" directory
-* sstring: version 1.5.5
+* sstring: version 1.5.6
 * 
 * MIT License
 * 
