508f4133ff2eb49dd9597eb1d30de9c94137fe75
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2022-03-13 14:24:46 +0530

    added `-p` flag for `mkdir`

--- samples/compile.sh
+++ samples/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir "./build"
+mkdir -p "./build"
 
 for f in *c; do
     gcc -g -W -Wall -Wextra -Wuninitialized -Wstrict-aliasing -ggdb3 -std=c17 -Wextra -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition $f -lm -o ./build/"$f".out
