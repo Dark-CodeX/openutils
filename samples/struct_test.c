96d5395f9588e51a723068e0881edd19b214fcfb
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-03-20 15:26:24 +0530

    machine changed

--- samples/struct_test.c
+++ samples/struct_test.c
@@ -10,6 +10,7 @@ int main(int argc, char const **argv)
 		vec.src[i].set(&vec.src[i], argv[i]);
 		vec.src[i].print(&vec.src[i], true, "\t\tLength = %llu, Entropy = %Lf", vec.src[i].length(&vec.src[i]), vec.src[i].entropy(&vec.src[i]));
 		vec.src[i].destructor(&vec.src[i]);
+		
 	}
 	vector_destructor(vec);
 	return 0;
