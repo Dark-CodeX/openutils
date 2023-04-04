d9db75f4fb253fa57a2efbdb39da187b89643abf
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-04-05 02:36:32 +0530

    some changes related to init of `heap_pair` class

--- samples/with_classes.cc
+++ samples/with_classes.cc
@@ -6,8 +6,15 @@ int main()
 {
 	using namespace openutils;
 
-	heap_pair<sstring, sstring> name("Tushar", "Chaurasia");
-	std::cout << "First name = `" << name.first() << "`\n";
-	std::cout << "Last name = `" << name.second() << "`\n";
+	std::cout << "NAME 1:\n";
+	heap_pair<sstring, sstring> name1("Ram", "Krishna"); // // 1st implementation
+	std::cout << "    First name = `" << name1.first() << "`\n";
+	std::cout << "    Last name = `" << name1.second() << "`\n\n";
+
+
+	heap_pair<sstring, sstring> name2 = heap_pair<sstring, sstring>::make_heap_pair("Tushar", "Chaurasia"); // 2nd implementation
+	std::cout << "NAME 2:\n";
+	std::cout << "    First name = `" << name2.first() << "`\n";
+	std::cout << "    Last name = `" << name2.second() << "`" << std::endl;
 	return 0;
 }
