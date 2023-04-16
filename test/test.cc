36459f82fdc6bb9bad349cd06c237adcf64f7ecc
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-04-16 20:48:54 +0530

    test file

--- test/test.cc
+++ test/test.cc
@@ -1,11 +1,29 @@
-#include "../sstring/sstring.hh"
-#include <cassert>
+#include <openutils/sstring/sstring.hh>
 #include <iostream>
 
-int main()
+constexpr static inline bool is_between(double value, double lower_bound, double upper_bound)
 {
-    using namespace openutils;
-    sstring x = "tushar tushar tushar";
-    x.rfind("tushar");
-    return 0;
+    return (value >= lower_bound && value <= upper_bound);
+}
+
+int main(int argc, const char **argv)
+{
+    if (argc == 1)
+    {
+        std::cerr << "err: no password passed" << std::endl;
+        return EXIT_FAILURE;
+    }
+    openutils::sstring x = argv[1];
+    double pass_strength = x.password_entropy();
+    if (is_between(pass_strength, 0, 28))
+        std::cout << "Very Weak Password" << std::endl;
+    else if (is_between(pass_strength, 29, 35))
+        std::cout << "Weak Password" << std::endl;
+    else if (is_between(pass_strength, 36, 59))
+        std::cout << "Medium Password" << std::endl;
+    else if (is_between(pass_strength, 60, 127))
+        std::cout << "Strong Password" << std::endl;
+    else
+        std::cout << "Very Strong Password" << std::endl;
+    return EXIT_SUCCESS;
 }
