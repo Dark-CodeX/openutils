451a1e04c377333d55e2d1567e245242cc247df9
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-04-17 18:02:59 +0530

    removed useless class declarations

--- samples/test.cc
+++ samples/test.cc
@@ -1,28 +1,23 @@
-#include "../returns/optional.hh"
-#include "../returns/default.hh"
+#include "../optional/optional.hh"
 #include <openutils/sstring/sstring.hh>
 #include <iostream>
 
-openutils::optional_t<openutils::sstring> test(openutils::sstring x)
+openutils::optional_t<openutils::sstring> test(const openutils::sstring &input)
 {
-	if (x.length() != 0)
-		return x.soundex();
+	if (input.length() != 0)
+		return input.soundex();
 	return openutils::optnull;
 }
 
-int main(void)
+int main()
 {
-	auto y = test(openutils::sstring::get_input());
+	openutils::optional_t<openutils::sstring> y = test(openutils::sstring::get_input());
 	if (y)
 		std::cout << y.get() << std::endl;
 	else
 	{
-		std::cerr << "value was null" << std::endl;
+		std::cerr << "err: value was (null)" << std::endl;
 		return EXIT_FAILURE;
 	}
-	openutils::sstring x = openutils::default_t();
-	int z = openutils::default_t();
-	std::cout << "Default value of sstring = `" << ((x.is_null() == true) ? "NULL" : x) << "`, with length = " << x.length() << std::endl;
-	std::cout << "Default value of int = " << z << std::endl;
 	return EXIT_SUCCESS;
 }
